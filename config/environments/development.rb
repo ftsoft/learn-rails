b483a647f34bd714f7c4bf0c239d0e9c4ad87c37
Frank Cooley <facooley@gmail.com>
2016-01-22 14:11:55 -0500

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,18 @@ Rails.application.configure do
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
+  config.action_mailer.smtp_settings = {
+  address: "smtp.mandrillapp.com",
+  port: 587,
+  user_name: Rails.application.secrets.mandrill_username,
+  password: Rails.application.secrets.mandrill_api_key
+}
+# Send email in development mode?
+config.action_mailer.perform_deliveries = true
+# ActionMailer Config
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
   config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
